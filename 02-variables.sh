ad0ed674213f27539adc41fc01e19f245b28ee3e
unknown <vvdswamy9479@gmail.com>
2024-02-16 14:50:01 +0530

    script changes

--- 02-variables.sh
+++ 02-variables.sh
@@ -6,4 +6,4 @@ PERSON2=Suresh
 echo "PERSON1 : Hi PERSON2, How r u?"
 echo "PERSON2 : Hi PERSON1, I'm fine, what about you?"
 echo "PERSON1 : I'm good what r u doing now?"
-echo "PERSON2 : I'm doing software job"s
+echo "PERSON2 : I'm doing software job"
