f1c68876efbe998d5e32b30338bb5a7ff3646404
unknown <vvdswamy9479@gmail.com>
2024-02-16 17:26:08 +0530

    4th scriptes

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hi $PERSON2, How r u?"
+echo "$PERSON2 : Hi $PERSON1, I'm fine, what about you?"
+echo "$PERSON1 : I'm good what r u doing now?"
+echo "$PERSON2 : I'm doing software job"
