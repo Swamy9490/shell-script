8d92630cff76f3b03161f76b371e284e167d7b44
unknown <vvdswamy9479@gmail.com>
2024-02-16 14:47:07 +0530

    script concepts

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh : Hi Suresh, How r u?"
-echo "Suresh : Hi Ramesh, I'm fine, what about you?"
-echo "Ramesh : I'm good what r u doing now?"
-echo "Suresh : I'm doing software job"s
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "PERSON1 : Hi PERSON2, How r u?"
+echo "PERSON2 : Hi PERSON1, I'm fine, what about you?"
+echo "PERSON1 : I'm good what r u doing now?"
+echo "PERSON2 : I'm doing software job"s
