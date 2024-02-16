77761ea41f8f1751c4d0fb06816df7d3d42d88d1
unknown <vvdswamy9479@gmail.com>
2024-02-16 20:45:50 +0530

    6th scriptes

---
+++ 06-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter Username:: "
+
+read -s USERBANE #the value entered above will be automaticall attached with variables
+
+echo "Please enter Password:: "
+read -s PASSWORD 
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
