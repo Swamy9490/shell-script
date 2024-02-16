bb2c27ba9732aad63a913ee2d5d20fdd48128390
unknown <vvdswamy9479@gmail.com>
2024-02-16 14:31:56 +0530

    2nd scripts

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : Hi Suresh, How r u?"
+echo "Suresh : Hi Ramesh, I'm fine, what about you?"
+echo "Ramesh : I'm good what r u doing now?"
+echo "Suresh : I'm doing software job"s
