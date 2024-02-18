164eec8e34a6409dd0d0fe1e93b09bc5464eb967
unknown <vvdswamy9479@gmail.com>
2024-02-18 06:59:45 +0530

    4th scriptes

---
+++ 08-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Mango" "Banana")
+
+echo "first value is: ${Fruits[0]}"
+
+echo "Second value is: ${Fruits[1]}"
+
+echo "Third value is: ${Fruits[2]}"
+
+echo "All Fruits is: ${Fruits[@]}"
