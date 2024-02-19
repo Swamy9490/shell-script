ecdfd0f8013255fc088760c0a00572b4b8acb340
unknown <vvdswamy9479@gmail.com>
2024-02-19 11:01:55 +0530

    4th scriptes

---
+++ 09-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo "Error:: Please run this script with root access"
+else
+    echo "You are root user"
+fi # means reverse of if,indicating condition end
+
+yum install mysql -y
