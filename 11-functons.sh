8f12a1c1bbb1e0d1bc64c10e957616137730f76f
unknown <vvdswamy9479@gmail.com>
2024-02-20 12:44:19 +0530

    4th scriptes

--- 11-functons.sh
+++ 11-functons.sh
@@ -2,9 +2,29 @@
 
 ID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+    echo "Error:: $2... Failed"
+    exit 1
+    else 
+    echo "$2... Success"
+    fi
+}
+
 if [ $ID -ne 0 ]
 then 
     echo "Error:: Please run this script with root access"
+    exit 1 # you can give other than 0
 else
     echo "You are root user"
-fi # means reverse of if,indicating condition end
+fi # fi means reverse of if,indicating condition end
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
+
