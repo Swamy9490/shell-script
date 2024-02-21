7fa5c0d8fb6e65ca7b92ff762ad05d03eab3f537
unknown <vvdswamy9479@gmail.com>
2024-02-21 16:05:55 +0530

    4th scriptes

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%D)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
-    echo "script name:: $0 "
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $ "
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-    echo "Error:: $2... Failed"
+    echo "Error:: $2... $R Failed $N"
     exit 1
     else 
-    echo "$2... Success"
+    echo "$2... $G Success $N"
     fi
 }
 
