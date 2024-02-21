ad2738fd6fa7a45c4fa602c864e30a6d032cedef
unknown <vvdswamy9479@gmail.com>
2024-02-21 09:27:25 +0530

    4th scriptes

--- practice-for-conditions.sh
+++ practice-for-conditions.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-ID=$( id -u )
+ID=$(id -u)
 
 VALIDATE(){
-if ($1 -ne 0)
+    if [ $1 -ne 0 ]
+    then
     echo "Error:: $2... Failed"
     exit 1
-else
-    echo "$2...Success"
+    else
+    echo "$2... Success"
     fi
 }
-if [ $ID -ne 0]
-echo "Error:: Please run this script with root access"
-exit 1 # please run this script with root access
 
+if [ $ID -ne 0 ]
+then
+    echo "Error:: Please run this script with root access"
+    exit 1 # you can give other than 0
 else
     echo "you are root user"
-    fi # fi means reverse of if indicating condition end
+fi # fi means reverse of if indicating condition end
 
 yum install MySQL -y
 
