f80f28a0f772a88cb88816eaa61e2b68ee195c16
unknown <vvdswamy7479@gmail.com>
2024-03-12 11:05:15 +0530

    roboshop-shell scripts

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.log" 
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
     echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){
