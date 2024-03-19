075c8c9b49396270c37b6941c6a1d69079f98c35
unknown <vvdswamy7479@gmail.com>
2024-03-19 21:55:14 +0530

    shell scripts

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -ht | grep -vE 'tmp|file')
-DISK_THRESOLD=1
+DISK_THRESHOLD=1
 message=""
 
 while IFS= read line
 do
     usage=$(echo line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo line | awk '{print $1F}')
-    if [ $usage -gt $DISK_THRESOLD ]
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
         message="High Disk Usage on $partition: $usage"
     fi
