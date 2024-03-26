05b09abc891928650f846281a574fdf378656cf3
unknown <vvdswamy7479@gmail.com>
2024-03-26 22:01:25 +0530

    shell scripts

--- 18-greetings.sh
+++ 18-greetings.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-PERSON=$1
+NAME=""
+WISHES=""
 
-echo "Hello $PERSON, im learning devops with aws"
+USAGE(){
+    echo "USAGE:: $(basename $0) -n<name> -w<wishes>"
+    echo "options"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. ex, Good Morning"
+    echo " -h, Display help and exit"
+}
+
+while getops ":n:w:h" opt; do
+    case $opt in
+    n) NAME="$OPTARG";;
+    w) WISHES="$OPTARG";;
+    h|*) USAGE; exit;;
+
+    esac
+done
